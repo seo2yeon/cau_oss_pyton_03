285a2c0a1e04077688cbe4ccce08169db4d3e59d
seo2yeon <sy040929@naver.com>
2023-05-19 20:43:48 +0900

    update

---
+++ figure_main.py
@@ -0,0 +1,32 @@
+# %%
+
+# [fill this line]
+"""
+import figure
+
+myline = figure.line(10)
+
+square = figure.area_square(myline.get_length())
+print(square)
+
+myline.set_length(20)
+regular_triangle = figure.area_regular_triangle(myline.get_length())
+print(regular_triangle)
+
+myline.set_length(30)
+circle = figure.area_circle(myline.get_length())
+print(circle)
+"""
+import figure
+
+myline = figure.line(10)
+square = figure.line.area_square(myline.get_length())
+print(square)
+
+myline.set_length(20)
+regular_triangle = figure.line.area_regular_triangle(myline.get_length())
+print(regular_triangle)
+
+myline.set_length(30)
+circle = figure.line.area_circle(myline.get_length())
+print(circle)
