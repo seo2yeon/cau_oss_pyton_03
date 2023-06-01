c494ff1b236c73228896e92f11502b575afb064c
seo2yeon <sy040929@naver.com>
2023-06-01 21:59:18 +0900

    implement hw5

--- figure_main.py
+++ figure_main.py
@@ -1,32 +1,22 @@
-# %%
-
-# [fill this line]
-"""
 import figure
 
-myline = figure.line(10)
+myline = figure.line(10,20)
 
-square = figure.area_square(myline.get_length())
-print(square)
+width, height = myline.get_length()
+try:
+    rectangle = figure.area_rectangle(width, height)
+    print(rectangle)
+except ValueError:
+    print("please input positive number for width and height")
 
-myline.set_length(20)
-regular_triangle = figure.area_regular_triangle(myline.get_length())
-print(regular_triangle)
+try:
+    ellipse = figure.area_ellipse(width, height)
+    print(ellipse)
+except ValueError:
+    print("please input positive number for width and height")
 
-myline.set_length(30)
-circle = figure.area_circle(myline.get_length())
-print(circle)
-"""
-import figure
-
-myline = figure.line(10)
-square = figure.line.area_square(myline.get_length())
-print(square)
-
-myline.set_length(20)
-regular_triangle = figure.line.area_regular_triangle(myline.get_length())
-print(regular_triangle)
-
-myline.set_length(30)
-circle = figure.line.area_circle(myline.get_length())
-print(circle)
+try:
+    right_triangle = figure.area_right_triangle(width, height)
+    print(right_triangle)
+except ValueError:
+    print("please input positive number for width and height")
